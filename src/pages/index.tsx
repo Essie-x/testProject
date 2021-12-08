9173a6e6d075892b5dd15b76e0f83dda7560ef61
Essie-x <747701776@qq.com>
2021-12-08 08:14:44 +0800

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import styles from './index.less';
 
 export default function IndexPage() {
+  if (1 === 1) console.log(2333);
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
