5453933e3d9fd00cf337411becb0cefb01f4f01a
Essie-x <747701776@qq.com>
2021-12-08 08:01:22 +0800

    test

---
+++ src2/.umi/core/plugin.ts
@@ -0,0 +1,8 @@
+// @ts-nocheck
+import { Plugin } from '/Users/bytedance/Desktop/testProject/node_modules/umi/node_modules/@umijs/runtime';
+
+const plugin = new Plugin({
+  validKeys: ['modifyClientRenderOpts','patchRoutes','rootContainer','render','onRouteChange','__mfsu','getInitialState','initialStateConfig','request',],
+});
+
+export { plugin };
---
+++ src2/.umi/plugin-helmet/exports.ts
@@ -0,0 +1,3 @@
+// @ts-nocheck
+// @ts-ignore
+export { Helmet } from '/Users/bytedance/Desktop/testProject/node_modules/react-helmet';
